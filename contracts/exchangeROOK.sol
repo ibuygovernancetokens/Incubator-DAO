41db5aea3c1e8f96f2f306c4e933de67715ef957
ibuygovernancetokens <ibuygovernancetokens@gmail.com>
2023-04-05 13:29:26 +0100

    Update with new process

--- contracts/exchangeROOK.sol
+++ contracts/exchangeROOK.sol
@@ -1324,9 +1324,9 @@ library SafeMath {
 
 pragma solidity 0.8.12;
 
-/// @title Migration contract from ROOK to pROOK
+/// @title Exchange contract from ROOK to pROOK and USDC
 /// @author IANAL
-contract Migration is Ownable, Pausable {
+contract ExchangeROOK is Ownable, Pausable {
 	using SafeMath for uint256;
 	using SafeERC20 for ERC20;
 
@@ -1336,17 +1336,27 @@ contract Migration is Ownable, Pausable {
 	/// @notice pROOK
 	ERC20 public tokenPROOK;
 
-	/// @notice emitted when migrate ROOK to pROOK
-	event Migrate(address indexed user, uint256 amount);
+	/// @notice USDC
+	ERC20 public tokenUSDC;
+
+	/// @notice Exchange Rate
+	uint256 public exchangeRate;
+
+	/// @notice emitted when exchange ROOK to pROOK and USDC
+	event Exchange(address indexed user, uint256 amount, uint256 value);
 
 	//
 	// @notice constructor
 	// @param _tokenROOK ROOK token address
 	// @param _tokenPROOK pROOK token address
+	// @param _tokenUSDC USDC token address
+	// @param _exchangeRate exchange rate value with 4 decimals
 	//
-	constructor(ERC20 _tokenROOK, ERC20 _tokenPROOK) Ownable() {
+	constructor(ERC20 _tokenROOK, ERC20 _tokenPROOK, ERC20 _tokenUSDC, uint256 _exchangeRate) Ownable() {
 		tokenROOK = _tokenROOK;
 		tokenPROOK = _tokenPROOK;
+		tokenUSDC = _tokenUSDC;
+		exchangeRate = _exchangeRate;
 		_pause();
 	}
 
@@ -1359,6 +1369,14 @@ contract Migration is Ownable, Pausable {
 	}
 
 	/**
+	 * @notice Exchange Rate value with 4 decimals, example 455000 = $45.50
+	 * @param _exchangeRate to use
+	 */
+	function setExchangeRate(uint256 _exchangeRate) external onlyOwner {
+		exchangeRate = _exchangeRate;
+	}
+
+	/**
 	 * @notice Withdraw ERC20 token
 	 * @param _token address for withdraw
 	 * @param _amount to withdraw
@@ -1368,13 +1386,15 @@ contract Migration is Ownable, Pausable {
 	}
 
 	/**
-	 * @notice Migrate from ROOK to pROOK
-	 * @param _amount of ROOK
+	 * @notice Exchange from ROOK to pROOK and USDC
+	 * @param _amount of ROOK exchanged
 	 */
-	function migrateROOK(uint256 _amount) external whenNotPaused {
+	function exchange(uint256 _amount) external whenNotPaused {
 		tokenROOK.safeTransferFrom(_msgSender(), address(this), _amount);
+		uint256 _value = _amount.mul(exchangeRate).div( 10000000000000000 );
 		tokenPROOK.safeTransfer(_msgSender(), _amount);
+		tokenUSDC.safeTransfer(_msgSender(), _value);
 
-		emit Migrate(_msgSender(), _amount);
+		emit Exchange(_msgSender(), _amount, _value);
 	}
 }
