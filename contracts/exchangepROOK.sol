1aff106b2990d99a8ed7a7e1502ab6935ef7d662
ibuygovernancetokens <ibuygovernancetokens@gmail.com>
2023-04-13 08:46:21 +0100

    Fix for deploy

--- contracts/exchangepROOK.sol
+++ contracts/exchangepROOK.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.19;
 
-import "openzeppelin-contracts/access/Ownable.sol";
-import "openzeppelin-contracts/token/ERC20/utils/SafeERC20.sol";
-import "openzeppelin-contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
 
 /// @title Exchange contract from pROOK to USDC
 /// @author IANAL
--- contracts/prook.sol
+++ contracts/prook.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.19;
 
-import "openzeppelin-contracts/token/ERC20/ERC20.sol";
-import "openzeppelin-contracts/access/Ownable.sol";
-import "openzeppelin-contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
 
 contract pROOK is ERC20, Ownable, ERC20Permit {
     constructor() ERC20("pROOK", "pROOK") ERC20Permit("pROOK") {}
